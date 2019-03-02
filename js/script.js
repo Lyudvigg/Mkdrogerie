f7a770edca6f18964c58bcc02819a6f4fa5cd5b3
Lyudvigg <grigoryanl486@gmail.com>
2019-03-02 15:56:26 +0400

    add dropdown

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 //for documents collaps
-$(function() {
-    var Accordion = function(el, multiple) {
+$(function () {
+    var Accordion = function (el, multiple) {
         this.el = el || {};
         this.multiple = multiple || false;
 
         // Variables privadas
         var links = this.el.find('.shop_left_menu_link');
         // Evento
-        links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+        links.on('click', { el: this.el, multiple: this.multiple }, this.dropdown)
     }
 
-    Accordion.prototype.dropdown = function(e) {
+    Accordion.prototype.dropdown = function (e) {
         var $el = e.data.el;
         $this = $(this),
             $next = $this.next();
@@ -25,3 +25,16 @@ $(function() {
 
     var accordion = new Accordion($('#accordion'), false);
 });
+
+/*==================Range==================*/
+
+
+$('#inp_range_block_first_inp').on('input', function () {
+    let inp_range_block_first_inp = $('#inp_range_block_first_inp').val();
+    $('.inp_range_block_first_inp').text(inp_range_block_first_inp);
+})
+
+$('#inp_range_block_second_inp').on('input', function () {
+    let inp_range_block_first_inp = $('#inp_range_block_second_inp').val();
+    $('.inp_range_block_second_inp').text(inp_range_block_first_inp);
+})
