e5f62af3735ab64f6b7ef35fc6871772dbd91275
Lyudvigg <grigoryanl486@gmail.com>
2019-03-03 17:08:11 +0400

    add functionality

--- js/script.js
+++ js/script.js
@@ -38,3 +38,60 @@ $('#inp_range_block_second_inp').on('input', function () {
     let inp_range_block_first_inp = $('#inp_range_block_second_inp').val();
     $('.inp_range_block_second_inp').text(inp_range_block_first_inp);
 })
+
+
+
+
+/*=================Card Functionality==================*/
+
+$('.card_btn').on('click', function () {
+    $(this).addClass('d_none');
+    $(this).parent().children('.card_block_functionality.d_none').removeClass('d_none');
+    $(this).parent().children('.card_block_functionality').addClass('card_block_functionality_active');
+});
+
+$('.plus_block').on('click', function () {
+    let count = $(this).parent().children('.count_block').children('.count').text();
+    count++;
+    $(this).parent().children('.count_block').children('.count').text(count);
+    let a = $(this).parent().children('.count_block').children('.count').text();
+    if (a == 1) {
+        $(this).parent().children('.minuse_block').addClass('d_none');
+        $(this).parent().children('.delete_icon_block').removeClass('d_none')
+    }
+    else if (a => 1) {
+        $(this).parent().children('.minuse_block').removeClass('d_none');
+        $(this).parent().children('.delete_icon_block').addClass('d_none');
+    }
+})
+
+$('.minuse_block').on('click', function () {
+    let count = $(this).parent().children('.count_block').children('.count').text();
+    count--;
+    $(this).parent().children('.count_block').children('.count').text(count);
+    let a = $(this).parent().children('.count_block').children('.count').text();
+    console.log(a);
+    if (a == 1) {
+        $(this).parent().children('.minuse_block').addClass('d_none');
+        $(this).parent().children('.delete_icon_block').removeClass('d_none');
+    }
+    else if (a => 1) {
+        $(this).parent().children('.minuse_block').removeClass('d_none');
+        $(this).parent().children('.delete_icon_block').addClass('d_none');
+    }
+})
+
+$('.delete_icon_block').on('click', function () {
+    $(this).parent().addClass('d_none');
+    $(this).parent().parent().children('.card_btn').removeClass('d_none');
+    $(this).parent().parent().children('.card_block_functionality.card_block_functionality_active').removeClass('card_block_functionality_active');
+})
+
+$('.card').on('click', function () {
+    if ($('.card_block_functionality').hasClass('card_block_functionality_active')) {
+        console.log(123)
+    }
+
+    console.log($('.card_block_functionality').hasClass('card_block_functionality_active'));
+    if()
+})
